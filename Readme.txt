00840cf95188720596ca0a5276fda02255106ea4
sajadulakash <sajadulislam1230@gmail.com>
2024-07-12 08:06:39 +0600

    commit1

---
+++ Readme.txt
@@ -0,0 +1,15 @@
+Step 01: Download the Zip file and unzip the file 
+Step 02: Install python 
+Step 03: Install VS code and open the project folder using VS code
+Step 04: open the terminal of vs code and run the given commands
+Step 05: Install the necessary libraries-
+	-pip install flask
+	-pip install joblib
+	-pip install numpy
+	-pip install scikit-learn 
+	-pip install -U scikit-learn==1.3.2
+Step 06: open app.py and go to the line number 29. After that change the model path according your project folder location. remember you need to use "/" rather than "\".
+Step 07: Run the app.py file
+Step 08: open the local server (http://127.0.0.1:7384)
+
+		
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import joblib
+import os
+import  numpy as np
+#import pickle
+
+app= Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route("/result",methods=['POST','GET'])
+def result():
+    gender=int(request.form['gender'])
+    age=int(request.form['age'])
+    hypertension=int(request.form['hypertension'])
+    heart_disease = int(request.form['heart_disease'])
+    ever_married = int(request.form['ever_married'])
+    work_type = int(request.form['work_type'])
+    Residence_type = int(request.form['Residence_type'])
+    avg_glucose_level = float(request.form['avg_glucose_level'])
+    bmi = float(request.form['bmi'])
+    smoking_status = int(request.form['smoking_status'])
+
+    x=np.array([gender,age,hypertension,heart_disease,ever_married,work_type,Residence_type,
+                avg_glucose_level,bmi,smoking_status]).reshape(1,-1)
+
+    model_path=os.path.join('C:/Users/sajadulakash/Desktop/Last Semester/cse299/stroke_prediction/','models/model_gb')
+    dt=joblib.load(model_path)
+
+    Y_pred=dt.predict(x)
+
+    # for No Stroke Risk
+    if Y_pred==0:
+        return render_template('nostroke.html')
+    else:
+        return render_template('stroke.html')
+
+if __name__=="__main__":
+    app.run(debug=True,port=7384)
